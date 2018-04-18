b4e350469e83137c349491df2746b227cd3d69ee
David Bishai <david.bishai@asurion.com>
2018-04-18 11:29:31 -0500

    remove vocab_question_4 since it is not used

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# machine-learning
+# Machine Learning Repo
 machine learning models and scripts
--- projects/ae_onboarding_classifier/utils/write_vocab_files.py
+++ projects/ae_onboarding_classifier/utils/write_vocab_files.py
@@ -7,7 +7,7 @@ SAVE_DIR = '../data'
 
 data_dir = os.path.join("..", "data")
 
-for i in range(1, 5):
+for i in range(1, 4):
     x_train, x_test, y_train, y_test = get_data(data_dir, i, preprocess=True)
     vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(
         MAX_DOCUMENT_LENGTH)
