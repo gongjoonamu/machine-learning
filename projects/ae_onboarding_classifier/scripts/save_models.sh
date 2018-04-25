60f38f610e45fce9bb04574de797052fe9554f13
David Bishai <david.bishai@asurion.com>
2018-04-25 14:35:55 -0500

    remove comment on line 4

--- projects/ae_onboarding_classifier/scripts/save_models.sh
+++ projects/ae_onboarding_classifier/scripts/save_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for Q in {1..3}
 do
-    #python3 ../bow_model.py $Q $1 -i ../data
+    python3 ../bow_model.py $Q $1 -i ../data
     FILE_NAME=model_question_$Q
     tar -czf $1/$FILE_NAME.tar.gz  $1/$FILE_NAME
 done
