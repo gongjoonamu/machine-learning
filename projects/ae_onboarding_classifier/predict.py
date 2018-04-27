d53f2735c6acd9e613031473f107fe4cf07bfbc8
David Bishai <david.bishai@asurion.com>
2018-04-27 12:51:45 -0500

    add dot to file paths

--- projects/ae_onboarding_classifier/predict.py
+++ projects/ae_onboarding_classifier/predict.py
@@ -14,8 +14,8 @@ def _create_example(features):
 
 class Predict:
     def __init__(self, question):
-        model_dir = os.path.join(SAVED_MODELS_DIR, get_model_name(question))
-        vocab_file = os.path.join(DATA_DIR, get_vocab_file(question))
+        model_dir = os.path.join('.', SAVED_MODELS_DIR, get_model_name(question))
+        vocab_file = os.path.join('.', DATA_DIR, get_vocab_file(question))
         self._vocab_processor = vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor.restore(
             vocab_file)
         self._predictor = tf.contrib.predictor.from_saved_model(model_dir)
