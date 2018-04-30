455e4a4a64b0cd351a26cc41c04c52a456a47f27
David Bishai <david.bishai@asurion.com>
2018-04-30 16:13:36 -0500

    remove unneeded reqs, hardcode nltk stopwords, add /src to paths

--- projects/ae_onboarding_classifier/utils/pre_processing.py
+++ projects/ae_onboarding_classifier/utils/pre_processing.py
@@ -3,16 +3,16 @@ import re
 import os
 from sklearn.model_selection import train_test_split
 import numpy as np
-from nltk.corpus import stopwords
 
 FILE_NAME = "anywhere_expert_transcripts.csv"
+# dumped from NLTK stopwords
+STOPS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 def _filter_words(words):
-    stops = stopwords.words("english")
     # remove non-alphanumeric symbols
     words = re.sub('[^A-Za-z0-9]', ' ', words).lower().split(' ')
     for i in range(len(words)):
-        if words[i] in stops:
+        if words[i] in STOPS:
             words[i] = ''
     return words
 
